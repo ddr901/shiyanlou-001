f5a3436ab4d6cf75e8d73231f83fff29405b80da
ddr901 <ddr901@qq.com>
2017-11-21 19:00:55 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 import csv
 from collections import namedtuple
@@ -7,7 +8,7 @@ IncomeTaxQuickLookupItem = namedtuple(
     ['start_point', 'tax_rate', 'quick_subtractor']
 )
 
-jibengongzi = 3500
+INCOME_TAX_START_POINT = 3500
 
 INCOME_TAX_QUICK_LOOKUP_TABLE = [
     IncomeTaxQuickLookupItem(80000, 0.45, 13505),
@@ -32,91 +33,120 @@ class Args(object):
         except (ValueError, IndexError):
             print('Parameter Error')
             exit()
-        @property
-        def config_path(self):
-            return self._value_after_option('-c')
 
-        @property
-        def userdata_path(self):
-            return self._value_after_option('-d')
+    @property
+    def config_path(self):
+        return self._value_after_option('-c')
 
-        @property
-        def export_path(self):
-            return self._value_after_option('-o')
+    @property
+    def userdata_path(self):
+        return self._value_after_option('-d')
+
+    @property
+    def export_path(self):
+        return self._value_after_option('-o')
+
 
 args = Args()
 
+
 class Config(object):
+
     def __init__(self):
-        self._config = {}
-        with open('d:\config.cfg','r') as f:
-            for s in f.readlines() :
-                (key ,value ) = s.strip().split(' = ')
+        self.config = self._read_config()
+
+    def _read_config(self):
+        config_path = args.config_path
+        config = {}
+        with open(config_path) as f:
+            for line in f.readlines():
+                key, value = line.strip().split(' = ')
                 try:
-                    self._config[id] = float(value)
+                    config[key] = float(value)
                 except ValueError:
-                    print('ValueError')
+                    print('Parameter Error')
                     exit()
-    def _get_config(self,key):
-        return self._config[key]
-    @property
-    def JiShuL(self):
-        return self._config('JiShuL')
+        return config
+
+    def _get_config(self, key):
+        try:
+            return self.config[key]
+        except KeyError:
+            print('Config Error')
+            exit()
 
     @property
-    def JiShuH(self):
-        return self._config('JiShuH')
+    def social_insurance_baseline_low(self):
+        return self._get_config('JiShuL')
 
     @property
-    def fivexian(self):
+    def social_insurance_baseline_high(self):
+        return self._get_config('JiShuH')
+
+    @property
+    def social_insurance_total_rate(self):
         return sum([
             self._get_config('YangLao'),
             self._get_config('YiLiao'),
             self._get_config('ShiYe'),
             self._get_config('GongShang'),
             self._get_config('ShengYu'),
-            self._get_config('GongJiJin')])
+            self._get_config('GongJiJin')
+        ])
+
 
 config = Config()
 
-class UserData(object):
-    def __init__(self):
-        userdata = []
-        with open('d:\cuser.csv','r') as f:
-            for s in f.readlines():
-                (employee_id, income_string) = s.strip().split(',')
 
+class UserData(object):
+
+    def __init__(self):
+        self.userdata = self._read_users_data()
+
+    def _read_users_data(self):
+        userdata_path = args.userdata_path
+        userdata = []
+        with open(userdata_path) as f:
+            for line in f.readlines():
+                employee_id, income_string = line.strip().split(',')
                 try:
-                    gongzi = int(income_string)
+                    income = int(income_string)
                 except ValueError:
-                    print('ValueError')
+                    print('Parameter Error')
                     exit()
-                userdata.append((employee_id,gongzi))
+                userdata.append((employee_id, income))
         return userdata
+
     def __iter__(self):
         return iter(self.userdata)
 
+
 class IncomeTaxCalculator(object):
-    def __init__(self,userdata):
+
+    def __init__(self, userdata):
         self.userdata = userdata
+
     @staticmethod
-    def get_shehuibaoxian(gongzi):
-        if gongzi < config.JiShuL():
-            return config.JiShuL() * config.fivexian()
-        if gongzi >config.JiShuH():
-            return config.JiShuH() * config.fivexian()
-        return gongzi * config.fivexian()
+    def calc_social_insurance_money(income):
+        if income < config.social_insurance_baseline_low:
+            return config.social_insurance_baseline_low * \
+                config.social_insurance_total_rate
+        if income > config.social_insurance_baseline_high:
+            return config.social_insurance_baseline_high * \
+                config.social_insurance_total_rate
+        return income * config.social_insurance_total_rate
+
     @classmethod
-    def gerensuodeshui(cls,gongzi):
-        shehuibaoxian = cls.get_shehuibaoxian(gongzi)
-        shiji_gongzi = gongzi - shehuibaoxian
-        nashui = shiji_gongzi - jibengongzi
-        if nashui <= 0:
-            return '0.00', '{:.2f}'.format(shiji_gongzi)
+    def calc_income_tax_and_remain(cls, income):
+        social_insurance_money = cls.calc_social_insurance_money(income)
+        real_income = income - social_insurance_money
+        taxable_part = real_income - INCOME_TAX_START_POINT
+        if taxable_part <= 0:
+            return '0.00', '{:.2f}'.format(real_income)
         for item in INCOME_TAX_QUICK_LOOKUP_TABLE:
-            if nashui > item.start_point:
-                tax = nashui * item.tax_rate - item.quick_subtractor
-                return '{:.2f}'.format(tax), '{:.2f}'.format(shiji_gongzi - tax)
+            if taxable_part > item.start_point:
+                tax = taxable_part * item.tax_rate - item.quick_subtractor
+                return '{:.2f}'.format(tax), '{:.2f}'.format(real_income - tax)
 
     def calc_for_all_userdata(self):
         result = []
@@ -130,11 +160,11 @@ class IncomeTaxCalculator(object):
 
     def export(self, default='csv'):
         result = self.calc_for_all_userdata()
-        with open('d:\gongzi.csv', 'w', newline='') as f:
+        with open(args.export_path, 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerows(result)
 
-#if __name__ == '__main__':
-    #calculator = IncomeTaxCalculator(UserData())
-    #calculator.export()
-    
+
+if __name__ == '__main__':
+    calculator = IncomeTaxCalculator(UserData())
+    calculator.export()
