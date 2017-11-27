349ec292aa9f0ed15cc925c34e640d7df0d1b4a5
ddr901 <ddr901@qq.com>
2017-11-28 00:06:02 +0800

    updata

--- app.py
+++ app.py
@@ -2,12 +2,45 @@
 # -*- coding:utf-8 -*-
 
 from flask import Flask,render_template,request,abort
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column,Integer,String
+from datetime import datetime
 import json
 import os
 
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql://root@localhost/shiyanlou'
+db = SQLAlchemy(app)
+
+class File(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    created_time = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+    category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
+    category = db.relationship('Category',
+            backref = db.backref('files', lazy='dynamic'))
+    def __init__(self,title,content,category,created_time=None):
+        self.title = title
+        self.content = content
+        if created_time is None:
+            created_time = datatime.utcnow()
+        self.created_time = created_time
+        self.category = category
+    def __repr__(self):
+        return '<File %r>'% self.title
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+   
+    def __init__(self,name):
+        self.name = name
+    
+    def __repr__(self):
+        return '<Category %r>'% self.name
 
 @app.route('/')
 def index():
